f8b1adf583059c8e678156aa3246c70441951742
pavloskii <pavlo.igor@gmail.com>
2018-06-09 17:42:02 +0200

    simple webapp done

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="app">
+    <div class="navbar has-shadow">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">MyCompany</a>
+
+          <span class="navbar-burger" v-on:click="toggleNav" v-bind:class="{ 'is-active': isActive }">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+
+        <div class="navbar-end navbar-menu" v-bind:class="{ 'is-active': isActive }">
+
+          <router-link to="/" class="navbar-item r-item">Home</router-link>
+          <router-link to="faq" class="navbar-item r-item">Features</router-link>
+          <router-link to="faq" class="navbar-item r-item">About</router-link>
+          <router-link to="faq" class="navbar-item r-item">FAQ</router-link>
+
+          <div class="navbar-item">
+            <p class="control">
+              <a class="button is-primary is-outlined">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+                <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <router-view></router-view>
+
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p>And this right here is a spiffy footer, where you can put stuff.</p>
+          </div>
+          <div class="column has-text-right">
+            <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
+            <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data: function() {
+    return {
+      isActive: false
+    };
+  },
+  methods: {
+    toggleNav: function() {
+      this.isActive = !this.isActive;
+    }
+  }
+};
+</script>
+
+<style lang="sass">
+@import '../node_modules/bulma/bulma.sass'
+@import 'mq'
+
+//My css
+.has-custom-shadow
+  border: 1px solid #dfe6f1
+  box-shadow: 0 0 7px 0 rgba(0, 0, 0, 0.05)
+    
+
+
+//Navigation and footer css
+.navbar
+  background-color: #383838
+  a:hover
+    color: gray
+
+.navbar-brand a 
+  color: #fff
+  font-weight: bold
+
+a.r-item
+  color:#C1C1C1
+  padding: 0.5rem 1.75rem
+  +mobile
+    color: gray
+    &:hover
+      background-color: #F1F1F1
+
+.navbar-burger
+  &:hover
+    background-color: #F1F1F1
+  span
+    background-color: #C1C1C1
+
+footer
+  background-color: $primary !important
+  color: #fff
+
+.icon
+  color: #fff
+  margin-left: 20px
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Faq from '@/components/Faq'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/faq',
+      name: 'Faq',
+      component: Faq
+    }
+  ]
+})
